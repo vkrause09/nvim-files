895683081d38d68de3bb884adb22a9de9503748f
vic <victorkrause9@gmail.com>
2026-02-22 18:51:25 -0600

    Updating lazy

--- lua/name/plugins/init.lua
+++ lua/name/plugins/init.lua
@@ -2,7 +2,6 @@ return {
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("name.plugins.telescope")
